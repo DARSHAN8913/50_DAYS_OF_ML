5bd40eb4be8cb43284b24fd085f5d5625b30cbd3
DARSHAN8913 <ddshatagar@gmail.com>
2024-06-07 12:45:41 +0530

    at 1245

--- Python_Basics/DATA-SICENCE-COURSE/print_stats.py
+++ Python_Basics/DATA-SICENCE-COURSE/print_stats.py
@@ -1,9 +1,12 @@
-print("vb")
-print("-------------------------")
-print("hi",end=" ")
-print("how",end=" ")
-print("r u",end=" ")
+# print("vb")
+# print("-------------------------")
+# print("hi",end=" ")    # end=  specifies the ending symbol
+# print("how",end=" ")
+# print("r u",end=" ")
 # O/P:
 # vb
 # -------------------------
 # hi how r u 
+print("a","b","c",sep="+")  #sep=  specifies seperator element
+# O/P:
+# a+b+c
