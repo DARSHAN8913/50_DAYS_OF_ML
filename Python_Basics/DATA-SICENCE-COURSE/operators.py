d69ff060c213b768bd28037d46ec23ed16a09868
DARSHAN8913 <ddshatagar@gmail.com>
2024-06-08 13:59:14 +0530

    cf

--- Python_Basics/DATA-SICENCE-COURSE/operators.py
+++ Python_Basics/DATA-SICENCE-COURSE/operators.py
@@ -1,12 +1,50 @@
-#ADDITION:
-print(2+3)
-print(4.5+9)
-print(True+True)
-print(True+5)
-print(7.45+True)
+# #ADDITION:
+# print(2+3)
+# print(4.5+9)
+# print(True+True)
+# print(True+5)
+# print(7.45+True)
+# print("-------------------------------------------------------------")
+# # O/P:
+# # 5
+# # 13.5
+# # 2        "True" is considered int 1 in arithmetic ops and false as 0
+# # 6
+# # 8.45
+# # print(3+"bee")
+# # O/P:
+# # error  bcoz int and str cant be added but,
+# print("hi"+" there")
+# # O/P:          STR     concatenation occurs if '+' is used
+# # hi there
+# print("-------------------------------------------------------------")
+
+# #SUBTRACTION:
+# print(False-True)
+# print("hi"-" there")
 # O/P:
-# 5
-# 13.5
-# 2        "True" is considered int 1 in arithmetic ops and false as 0
-# 6
-# 8.45
+# -1
+#    print("hi"-" there")
+# TypeError: unsupported operand type(s) for -: 'str' and 'str'
+# print("-------------------------------------------------------------")
+
+#MULTIPLICATION & DIV:
+# same rules as subtraction
+
+#FLOOR DIVISION:
+# print(3//2)
+# print(2.5//5)    #Ommits the decimal part and return int
+# print(1//3)      # Or it return the quotient only
+# O/P:
+# 1
+# 0.0
+# 0
+
+#MODULO:
+# print(4%5)
+# print(6%4)
+# # O/P:
+# # 4
+# # 2
+
+
